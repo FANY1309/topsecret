b729eb4cbbc83f5255a6da75805da2b27bc8932b
Fany Martinez <estefania.marhen@gmail.com>
2023-06-15 12:06:10 +0100

    add functions
     another function to colisionated

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const catPlayer = document.getElementsByClassName("cat");
+const cactus = document.getElementsByClassName("floor-cactus");
 
 document.body.addEventListener("keyup", function (e) {
   if (e.key === " " ||
@@ -11,3 +12,15 @@ document.body.addEventListener("keyup", function (e) {
     }, 1000);
   }
 });
+
+setInterval(function () {
+  const catbottom = parseInt(window.getComputedStyle(catPlayer).getPropertyValue("bottom"));
+
+  const cactusLeft = parseInt(
+    window.getComputedStyle(".floor-cactus").getPropertyValue("left")
+  );
+
+  if (cactusLeft <  && cactusLeft > 134 && catbottom >= 195) {
+    alert("Game Over!");
+  }
+}, 10);
