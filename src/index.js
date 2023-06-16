a81066f026e1e8715b036e86c92541b7f9973f57
Nirwita <acasdigitalwork@gmail.com>
2023-06-16 17:11:18 +0000

    Nuevo commit 3 punto 0
    Añadidas las imágenes nuevas y arreglada la función COLISIÓN

--- src/index.js
+++ src/index.js
@@ -2,30 +2,30 @@ const cat = document.getElementById("cat");
 const obstacle = document.getElementById("obstacle");
 
 function catJump() {
-  if (cat.classList != "catJump") {
-    cat.classList.add("catJump");
+  if (cat.classList != "jump") {
+    cat.classList.add("jump");
 
     setTimeout(function () {
-      cat.classList.remove("catJump");
-    }, 300);
+      cat.classList.remove("jump");
+    }, 500);
   }
 }
 
-// const isAlive = setInterval(function () {
-//   // get current cat Y position
-//   const catTop = parseInt(window.getComputedStyle(cat).getPropertyValue("top"));
+const isAlive = setInterval(function () {
+  // get current cat Y position
+  const catBottom = parseInt(window.getComputedStyle(cat).getPropertyValue("bottom"));
 
-//   // get current obstacle X position
-//   const obstacleLeft = parseInt(
-//     window.getComputedStyle(obstacle).getPropertyValue("left")
-//   );
+  // get current obstacle X position
+  const obstacleLeft = parseInt(
+    window.getComputedStyle(obstacle).getPropertyValue("left")
+  );
 
-//   // detect collision
-//   if (obstacleLeft < 50 && obstacleLeft > 0 && catTop >= 590) {
-//     // collision
-//     alert("Game Over!");
-//   }
-// }, 10);
+  // detect collision
+  if (obstacleLeft < 179 && obstacleLeft > 0 && catBottom >= 200) {
+    // collision
+    alert("Game Over!");
+  }
+}, 10);
 
 document.addEventListener("keydown", function (event) {
   catJump();
