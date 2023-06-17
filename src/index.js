187dd4e69925a6469bb1e3b2e7d6fdfb83668e9a
Pablo Santana <pablosntojd@gmail.com>
2023-06-17 18:03:54 +0100

    finished game functionality TODO: change collision px

--- src/index.js
+++ src/index.js
@@ -6,48 +6,55 @@ const score = document.querySelector(".score");
 const gameOver = document.querySelector(".game-over");
 
 let playerScore = 0;
+let interval = 0;
 
-const scoreCounter = () => {
-  playerScore++;
-  score.innerHTML = `${playerScore}`;
-};
+function catJump() {
+  const scoreCounter = () => {
+    playerScore++;
+    score.innerHTML = `${playerScore}`;
+  };
 
-document.addEventListener("keydown", (start) => {
-  if (start.code === "Space") {
-    gameOver.style.display = "none";
-    catPlayer.classList.add("cat-running");
-    mummy.classList.add("obstacle-moving");
-    bg.style.animation = "animated-background 80s linear infinite";
-    floor.style.animation = "animated-floor 18s linear infinite";
-    playerScore = 0;
-    setInterval(scoreCounter, 200);
-  }
-});
-
-document.addEventListener("keydown", (e) => {
-  if (e.key === "ArrowUp" || e === TouchEvent) {
-    if (catPlayer.classList !== "cat cat-jump") {
-      catPlayer.classList.add("cat-jump");
+  document.addEventListener("keydown", (start) => {
+    if (start.code === "Space") {
+      gameOver.style.display = "none";
+      catPlayer.classList.add("cat-running");
+      mummy.classList.add("obstacle-moving");
+      bg.style.animation = "animated-background 80s linear infinite";
+      floor.style.animation = "animated-floor 18s linear infinite";
+      playerScore = 0;
+      interval = setInterval(scoreCounter, 200);
     }
-    setTimeout(() => {
-      catPlayer.classList.remove("cat-jump");
-    }, 700);
+  });
+
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowUp" || e === TouchEvent) {
+      if (catPlayer.classList !== "cat cat-jump") {
+        catPlayer.classList.add("cat-jump");
+      }
+      setTimeout(() => {
+        catPlayer.classList.remove("cat-jump");
+      }, 700);
+    }
+  });
+
+  function endGame() {
+    setInterval(() => {
+      const mummyLeft = parseInt(getComputedStyle(mummy).getPropertyValue("left"));
+      const catBottom = parseInt(getComputedStyle(catPlayer).getPropertyValue("bottom"));
+      // detect collision
+      if (mummyLeft <= 280 && mummyLeft >= 150 && catBottom <= 540) {
+        // collision
+        gameOver.style.display = "block";
+        catPlayer.classList.remove("cat-running");
+        mummy.classList.remove("obstacle-moving");
+        bg.style.animation = "none";
+        floor.style.animation = "none";
+        clearInterval(interval);
+        playerScore = 0;
+      }
+    }, 10);
   }
-});
+  endGame();
+}
 
-// const obstacle = document.querySelector("floor-cactus");
-// const isAlive = setInterval(function () {
-//   // get current cat Y position
-//   const catTop = parseInt(window.getComputedStyle(catPlayer).getPropertyValue("bottom"));
-
-//   // get current obstacle X position
-//   const obstacleLeft = parseInt(
-//     window.getComputedStyle(obstacle).getPropertyValue("left")
-//   );
-
-//   // detect collision
-//   if (obstacleLeft < 50 && obstacleLeft > 0 && catTop >= 590) {
-//     // collision
-//     alert("Game Over!");
-//   }
-// }, 10);
+catJump();
